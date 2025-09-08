fc20046d720a0c18d8cc2f8745b82a8ba444cae4
Bill Cauchois <wcauchois@gmail.com>
2025-09-08 15:05:04 -0400

    add some comments

--- packages/library/src/lib/componentOverrides.tsx
+++ packages/library/src/lib/componentOverrides.tsx
@@ -2,10 +2,27 @@ import { createContext, ReactNode, useContext } from "react"
 import { IconComponent } from "./IconComponent"
 
 export type ComponentOverrides = {
+	/**
+	 * Badge is a generic component that (in the default implementation)
+	 * renders some small text with a light grey rounded background.
+	 *
+	 * These are mostly placed next to nodes to display information
+	 * (like a nested file type) or provide additional actions (like
+	 * magnifying the value.)
+	 */
 	badge?: React.FC<{ children: ReactNode; onClick?: () => void }>
 
+	/**
+	 * Icon used when a value is collapsed for a button to expand the value.
+	 */
 	magnifyIcon?: IconComponent
 
+	/**
+	 * Icon used when an image URL is shown; user may hover over this icon
+	 * to show a preview of the image.
+	 *
+	 * (Only if the richPreviews plugin is active.)
+	 */
 	imageIcon?: IconComponent
 }
 
