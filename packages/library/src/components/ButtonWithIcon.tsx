f5d0f75f1faa75b78953004a97a42ef6903834db
Bill Cauchois <wcauchois@gmail.com>
2025-09-23 12:36:24 -0400

    library: add responsive mode for ButtonWithIcon

--- packages/library/src/components/ButtonWithIcon.tsx
+++ packages/library/src/components/ButtonWithIcon.tsx
@@ -2,23 +2,56 @@ import { IconComponent } from "../lib/IconComponent"
 import { Button, ButtonProps } from "./Button"
 import * as stylex from "@stylexjs/stylex"
 
+const SMALL_MEDIA = "@media (max-width: 768px)"
+
 const styles = stylex.create({
 	icon: {
 		marginRight: 4,
+	},
+
+	iconWithHiding: {
+		marginRight: {
+			[SMALL_MEDIA]: 0,
+		},
+	},
+
+	label: {
+		textWrap: "nowrap",
+	},
+
+	labelWithHiding: {
+		display: {
+			default: "block",
+			[SMALL_MEDIA]: "none",
+		},
 	},
 })
 
 export type ButtonWithIconProps = ButtonProps & {
 	icon: IconComponent
+	hideLabelOnMobile?: boolean
 }
 
 export function ButtonWithIcon(props: ButtonWithIconProps) {
-	const { icon: Icon, children, ...restProps } = props
+	const { icon: Icon, children, hideLabelOnMobile, ...restProps } = props
 
 	return (
 		<Button {...restProps}>
-			<Icon size={14} {...stylex.props(styles.icon)} />
-			{children}
+			<Icon
+				size={14}
+				{...stylex.props(
+					styles.icon,
+					hideLabelOnMobile && styles.iconWithHiding
+				)}
+			/>
+			<div
+				{...stylex.props(
+					styles.label,
+					hideLabelOnMobile && styles.labelWithHiding
+				)}
+			>
+				{children}
+			</div>
 		</Button>
 	)
 }
